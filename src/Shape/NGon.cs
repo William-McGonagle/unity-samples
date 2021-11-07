ab54ca355ffe45a1b343c75303d552d98db5cb0a
William McGonagle <wpmcgonagle@gmail.com>
2021-11-07 07:33:55 -0500

    ✨ Added NGon Generation

--- src/Shape/NGon.cs
+++ src/Shape/NGon.cs
@@ -5,15 +5,7 @@ using UnityEngine;
 public class NGon
 {
 
-    public static float Area(int n, float radius)
-    {
-
-        // Standard Forumula for Area of N-Gon
-        return (n * radius * radius) / (4 * Mathf.Tan(Mathf.PI / n));
-
-    }
-
-    public static float Area(int n, float width, float height)
+    private static float RadiusFromHeight(float height)
     {
 
         // Initialize Values
@@ -36,6 +28,24 @@ public class NGon
 
         }
 
+        return radius;
+
+    }
+
+    public static float Area(int n, float radius)
+    {
+
+        // Standard Forumula for Area of N-Gon
+        return (n * radius * radius) / (4 * Mathf.Tan(Mathf.PI / n));
+
+    }
+
+    public static float Area(int n, float width, float height)
+    {
+
+        // Convert Height to Radius
+        float radius = RadiusFromHeight(height);
+
         // Return the Area times Width and Height
         return Area(n, radius) * width * height;
 
@@ -44,6 +54,7 @@ public class NGon
     public static Mesh Generate(int n)
     {
 
+        // Return the Generated Mesh
         return Generate(n, 1, 1);
 
     }
@@ -51,16 +62,58 @@ public class NGon
     public static Mesh Generate(int n, float radius)
     {
 
-        // TODO: @William-McGonagle - Implement Mesh Generation
-        return null;
+        // Initialize Variables
+        float pointAngle = Mathf.PI / n;
+        Vector3[] vertices = new Vector3[n + 1];
+        int[] indicies = new int[n * 3];
+
+        // Middle Point
+        vertices[0] = new Vector3(0, 0, 0);
+
+        // Loop Through Border Points
+        for (int i = 0; i < n; i++)
+        {
+
+            // Add Vertices at Border Point
+            vertices[i + 1] = new Vector3(Mathf.Cos(n * i), Mathf.Sin(n * i), 0);
+
+            // Add Triangle to Connect
+            indicies[(3 * i) + 0] = 0;
+            indicies[(3 * i) + 1] = (i + 1);
+            indicies[(3 * i) + 2] = ((i + 1) % n) + 1;
+
+        }
+
+        // Create Mesh Object
+        Mesh output = new Mesh();
+
+        // Set Mesh Object Data
+        output.vertices = vertices;
+        output.triangles = indicies;
+
+        // Optimize Mesh
+        output.Optimize();
+
+        // Clean Up Mesh
+        output.RecalculateTangents();
+        output.RecalculateNormals();
+        output.RecalculateBounds();
+
+        // Output the Cleaned Mesh
+        return output;
 
     }
 
     public static Mesh Generate(int n, float width, float height)
     {
 
-        // TODO: @William-McGonagle - Implement Mesh Generation
-        return null;
+        // TODO: STRETCH THE MESH TO COVER WIDTH AND HEIGHT
+
+        // Convert Height to Radius
+        float radius = RadiusFromHeight(n, height);
+
+        // Return the Generated Mesh
+        return Generate(n, radius);
 
     }
 
